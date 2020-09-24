74282e515931e0f5ac66df19e6d9d69049251f14
Rhett Coleman <68975053+RhettColeman@users.noreply.github.com>
2020-09-23 20:57:37 -0400

    Latest update

--- train_reply_not_txt.py
+++ train_reply_not_txt.py
@@ -9,29 +9,32 @@ auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth, wait_on_rate_limit=True)
 
-FILE_NAME = 'last_seen.txt'
-
-def read_last_seen(FILE_NAME):
-    file_read = open(FILE_NAME, 'r')
-    last_seen_id = int(file_read.read().strip())
-    file_read.close()
-    return last_seen_id
-
-def store_last_seen(FILE_NAME, last_seen_id):
-    file_write = open(FILE_NAME, 'w')
-    file_write.write(str(last_seen_id))
-    file_write.close()
-    return
+# FILE_NAME = 'last_seen.txt'
+#
+# def retrieve_last_seen_id(FILE_NAME):
+#     f_read = open(FILE_NAME, 'r')
+#     last_seen_id = int(f_read.read().strip())
+#     f_read.close()
+#     return last_seen_id
+#
+# def store_last_seen_id(FILE_NAME, last_seen_id):
+#     f_write = open(FILE_NAME, 'w')
+#     f_write.write(str(last_seen_id))
+#     f_write.close()
+#     return
 
 def bot_instructions():
     print('Waiting for mentions...')
-    tweets = api.mentions_timeline(read_last_seen(FILE_NAME), tweet_mode='extended')
-    for tweet in reversed(tweets):
-        print(str(tweet.id) + '-' + tweet.full_text)
+    last_seen_id = retrieve_last_seen_id(FILE_NAME)
+    mentions = api.mentions_timeline(last_seen_id, tweet_mode='extended')
+    for mention in reversed(mentions):
+        if not mention:
+            return
+        print(str(mention.id) + '-' + mention.full_text, flush=True)
         print('Found @Daily Train Post', flush=True)
         print('fav-ing and retweeting tweet...', flush=True)
-        api.create_favorite(tweet.id)
-        api.retweet(tweet.id)
+        api.create_favorite(mention.id)
+        api.retweet(mention.id)
 
 def bot_reply():
     bot_instructions()
